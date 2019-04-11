32929d97b6f8f1a26f23eb2a0d5caf10b25fb510
강전혁 <jenhyuk0318@gmail.com>
2019-04-11 16:50:01 +0000

    proxy 설정

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 const express = require('express');
-
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
 
-  res.json(customers);
+
+app.get('/api/greet', (req, res) => {
+  res.send('Hi');
 });
+
 
 const port = 5000;
 
