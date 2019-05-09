f308c432d3f65e9323f122ecfa3ecde5e47b7cc2
강전혁 <jenhyuk0318@gmail.com>
2019-05-09 03:22:24 +0000

    server : main 코드 흐름 수정

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const databaseUrl = 'mongodb://14.38.25.223:27017/local';
 // 집
 //const databaseUrl = 'mongodb://localhost:27017/local';
 
-let database;
+
 
 
 const app = express();
@@ -27,9 +27,21 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 app.use('/upload', serveStatic(path.join(__dirname, 'upload')));
 
+app.use('/api/users', require('./api/users/users'));
+app.use('/api/auth', require('./api/auth/auth'));
+app.use('/api/file', require('./api/file/file'));
+
+
+
+const server = require('http').createServer(app).listen(app.get('port'), function(){
+	console.log('서버 시작. 포트 : ' + app.get('port'));
+	connectDB();
+});
+const io = require('./socket.js')(server);
 
 
 function connectDB(){
+	let database;
 	mongoose.Promise = global.Promise;
 	mongoose.connect(databaseUrl);
 	database = mongoose.connection;
@@ -40,18 +52,3 @@ function connectDB(){
 		console.log('데이터베이스 연결 성공');
 	});
 }
-
-
-app.use('/api/users', require('./api/users/users'));
-app.use('/api/auth', require('./api/auth/auth'));
-app.use('/api/file', require('./api/file/file'));
-
-
-
-
-const server = require('http').createServer(app).listen(app.get('port'), function(){
-	console.log('서버 시작. 포트 : ' + app.get('port'));
-	connectDB();
-});
-const io = require('./socket.js')(server);
-
